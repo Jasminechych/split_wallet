f370baccac536400eb9d44c0b231f1dba661518e
jasminechych <jasminechych@gmail.com>
2024-03-04 16:20:08 +0800

    feat: add functionality to sort records by date in the RecordList

--- src/components/RecordList/RecordList.jsx
+++ src/components/RecordList/RecordList.jsx
@@ -5,16 +5,18 @@ function RecordList({ data = [], handleUpdateRecord, handleDeleteRecord }) {
 	return (
 		<div className={style.recordList}>
 			{data.length > 0 ? (
-				data.map((item) => (
-					<RecordItem
-						key={item.id}
-						id={item.id}
-						title={item.billTitle}
-						date={item.billDate}
-						handleUpdateRecord={(id) => handleUpdateRecord(id)}
-						handleDeleteRecord={(id) => handleDeleteRecord(id)}
-					/>
-				))
+				data
+					.sort((a, b) => b.billDate.localeCompare(a.billDate))
+					.map((item) => (
+						<RecordItem
+							key={item.id}
+							id={item.id}
+							title={item.billTitle}
+							date={item.billDate}
+							handleUpdateRecord={(id) => handleUpdateRecord(id)}
+							handleDeleteRecord={(id) => handleDeleteRecord(id)}
+						/>
+					))
 			) : (
 				<h4 className={style.emptyContent}>。。。 還沒有任何紀錄喔 。。。</h4>
 			)}
